a61cca28ad52fd8cd000fd7c4b2a2fb68ba73080
dwaynediddy <burrows.dwayne@icloud.com>
2022-04-20 19:08:07 +0200

    basic form for posts added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import PostList from './features/posts/PostList'
+import AddPostForm from './features/posts/AddPostForm'
 
 import './App.css';
 
 function App() {
   return (
     <main className="App">
+      <AddPostForm />
       <PostList />
+
     </main>
   );
 }
---
+++ src/features/posts/AddPostForm.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import { nanoid } from '@reduxjs/toolkit'
+
+import { postAdded } from './postSlice'
+ 
+
+const AddPostForm = () => {
+    const dispatch = useDispatch()
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    
+    const onTitleChange = e =>(e.target.value)
+    const onContentChange = e =>(e.target.value)
+
+    const onSavePostClicked = () => {
+        if(title && content) {
+            dispatch(
+                postAdded({
+                    id: nanoid(),
+                    title,
+                    content,
+                })
+            )
+            setTitle('')
+            setContent('')
+        }
+    }
+
+  return (
+    <section>
+        <h2>Add Post</h2>
+        <form>
+            <label htmlFor="postTitle">Post Title: </label>
+            <input
+                type="text"
+                id="postTitle"
+                name="postTitle"
+                value={title}
+                onChange={onTitleChange}
+            />
+            <label htmlFor="postContent">Content: </label>
+            <textarea 
+                id="postContent"    
+                name="postContent" 
+                value={content}
+                onChange={onContentChange}
+            />
+            <button type='button' onClick={onSavePostClicked}>Submit Post</button>
+        </form>
+    </section>
+  )
+}
+
+export default AddPostForm
