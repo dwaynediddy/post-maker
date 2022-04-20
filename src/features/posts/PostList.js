eba4ba7d0197f3b7c9bbdccdf1d3b226cc9122bb
dwaynediddy <burrows.dwayne@icloud.com>
2022-04-20 20:34:09 +0200

    added posts to be in order from latest at top

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -6,7 +6,9 @@ import TimeAgo from '../posts/TimeAgo'
 const PostList = () => {
     const posts = useSelector(selectAllPosts)
 
-    const renderPosts = posts.map(post => (
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    const renderPosts = orderedPosts.map(post => (
       <article key={post.id}>
           <h3>{post.title}</h3>
           <p>{post.content.substring(0, 100)}</p>
